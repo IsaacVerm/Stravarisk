5ff19bb78aae36a65bc2b68c758f0b3eb9073484
isaac <isaacverm@proton.me>
2024-07-30 20:58:01 +0200

    use attribute player in places instead of color directly

--- app.py
+++ app.py
@@ -1,63 +1,24 @@
 from flask import Flask, render_template
-import random
+import sqlite3
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    # places is a list of countries featuring in the Risk game
-    places = [
-        "Alaska",
-        "Northwest Territory",
-        "Greenland",
-        "Alberta",
-        "Ontario",
-        "Quebec",
-        "Western United States",
-        "Eastern United States",
-        "Central America",
-        "Venezuela",
-        "Peru",
-        "Brazil",
-        "Argentina",
-        "Iceland",
-        "Scandinavia",
-        "Ukraine",
-        "Great Britain",
-        "Northern Europe",
-        "Western Europe",
-        "Southern Europe",
-        "North Africa",
-        "Egypt",
-        "East Africa",
-        "Congo",
-        "South Africa",
-        "Madagascar",
-        "Ural",
-        "Siberia",
-        "Yakutsk",
-        "Kamchatka",
-        "Irkutsk",
-        "Mongolia",
-        "China",
-        "Japan",
-        "Afghanistan",
-        "Middle East"
-    ]
+    # holdings contains the player who owns each place
+    # fetch both players and places from the database
+    # zip them together to create holdings, a list of dictionaries
+    # and pass holdings to the template
+    connection = sqlite3.connect('stravarisk.sqlite')
+    cursor = connection.cursor()
+    
+    places = [place[0] for place in cursor.execute("SELECT place FROM holdings").fetchall()]
+    players = [player[0] for player in cursor.execute("SELECT player FROM holdings").fetchall()]
 
-    # create a list of colors
-    # this list is as long as the places list
-    # there's a color for each player
-    # since we have 5 players, we have 5 colors
-    # the colors are either red, green, blue, yellow, or purple
-    player_colors = ["red", "green", "blue", "yellow", "purple"]
-    colors = random.choices(player_colors,
-                            k=len(places))
+    holdings = [{"place": place, "player": player} for place, player in zip(places, players)]
 
-    places = [{"name": place, "color": color} for place, color in zip(places, colors)]
-
-    return render_template("index.html", places=places)
+    return render_template("index.html", holdings=holdings)
 
 if __name__ == "__main__":
     app.run(debug=True)
