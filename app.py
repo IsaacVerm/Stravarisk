986b36382d18db784e2b9d1a43112c89a9047e33
isaac <isaacverm@proton.me>
2024-07-30 21:03:16 +0200

    add counter number of armies each place

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ def home():
     
     places = [place[0] for place in cursor.execute("SELECT place FROM holdings").fetchall()]
     players = [player[0] for player in cursor.execute("SELECT player FROM holdings").fetchall()]
+    count_armies = [army_count[0] for army_count in cursor.execute("SELECT count_armies FROM holdings").fetchall()]
 
-    holdings = [{"place": place, "player": player} for place, player in zip(places, players)]
+    holdings = [{"place": place, "player": player, "army_count": army_count} for place, player, army_count in zip(places, players, count_armies)]
 
     return render_template("index.html", holdings=holdings)
 
