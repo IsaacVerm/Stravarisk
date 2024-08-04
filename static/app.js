fe01a889fcf4dc5859f4568e6b378c37d41556cd
isaac <isaacverm@proton.me>
2024-08-04 14:12:31 +0200

    refactor selectPlace into renderPlace, handleSelectPlace and check functions

--- static/app.js
+++ static/app.js
@@ -21,65 +21,80 @@ async function initializeState() {
         .catch(error => console.error('Error fetching data:', error));
 }
 
-/*
-add places on load:
-- fetch data from /api/holdings
-- create an element for each place filled in with holding data
-*/
+function renderPlace(holding, placesElement) {
+    const placeElement = document.createElement('div');
+
+    placeElement.setAttribute('class', 'place');
+    placeElement.setAttribute('place', holding.place);
+    placeElement.setAttribute('player', holding.player);
+    placeElement.setAttribute('army_count', holding.army_count);
+
+    placeElement.textContent = holding.army_count;
+
+    placesElement.appendChild(placeElement);
+}
 
 function renderPlaces() {
     const placesElement = document.getElementById('places');
 
     state.holdings.forEach(holding => {
-        const placeElement = document.createElement('div');
-
-        placeElement.setAttribute('class', 'place');
-        placeElement.setAttribute('place', holding.place);
-        placeElement.setAttribute('player', holding.player);
-        placeElement.setAttribute('army_count', holding.army_count);
-
-        placeElement.textContent = holding.army_count;
-
-        placesElement.appendChild(placeElement);
+        renderPlace(holding, placesElement);
     });
 }
 
-/*
-if you click on a place:
-- the place is selected if the player owns it
-- an alert is shown if the player does not own the place
-- if you click on a place that is already selected, it's unselected
-*/
-function selectPlace(event) {
-    // get data needed to make decision what to select or unselect
-    const selectedPlace = event.target;
-    const placePlayer = selectedPlace.getAttribute('player');
+function checkPlaceOwned(place) {
+    return state.holdings.filter(holding => holding.place === place)[0].player === state.currentPlayer;
+}
 
-    // do you own the place selected?
-    // if not, an alert is shown
-    if (state.currentPlayer != placePlayer) {
+function increaseArmyCount(place) {
+    state.holdings.filter(holding => holding.place === place)[0].army_count++;
+}
+
+function decreaseArmyCount(place) {
+    state.holdings.filter(holding => holding.place === place)[0].army_count--;
+}
+
+function renderArmyCountPlace(place) {
+    // select place element
+    const placeElement = document.querySelector(`[place="${place}"]`);
+
+    // set army count as text content
+    placeElement.textContent = state.holdings.filter(holding => holding.place === place)[0].army_count;
+}
+
+function handleSelectPlace(event) {
+    const selectedPlace = event.target;
+    const place = selectedPlace.getAttribute('place');
+
+    const leftClick = event.button === 0;
+    const rightClick = event.button === 2;
+
+    if (leftClick|rightClick && !checkPlaceOwned(place)) {
         alert('You do not own this place!');
     }
 
-    // if you own the place
-    // but it's already selected, unselect it
-    if (state.currentPlayer === placePlayer && selectedPlace.classList.contains('selected')) {
-        selectedPlace.classList.remove('selected');
-        return
+    if (leftClick && checkPlaceOwned(place)) {
+        increaseArmyCount(place);
+        renderArmyCountPlace(place);
     }
 
-    // if you do own the place
-    // select the place if it's not already selected
-    if (state.currentPlayer === placePlayer && !selectedPlace.classList.contains('selected')) {
-        selectedPlace.classList.add('selected');
+    if (rightClick && checkPlaceOwned(place)) {
+        decreaseArmyCount(place);
+        renderArmyCountPlace(place);
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     initializeState().then(() => {
+        // the DOM containing the places element has to load before you can attach any event listeners to it
         renderPlaces();
-        // the DOM has to load with the places element before you can any event listeners to it
-        document.getElementById('places').addEventListener('click', selectPlace);
+
+        document.getElementById('places').addEventListener('click', handleSelectPlace);
+        document.addEventListener('contextmenu', event => {
+            event.preventDefault();
+            handleSelectPlace(event);
+        });
     });
 });
+
 
